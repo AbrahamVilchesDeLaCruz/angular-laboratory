94b1e4f498604159a4de2d0547f6e49c9f4d0a7b
abraham <abrahamvilchesdelacruz@gmail.com>
2024-11-27 05:57:58 +0100

    feat: Add authentication check on app component initialization and persist user at localstorage

--- lab/src/app/app.component.ts
+++ lab/src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from "@angular/core";
-import { RouterOutlet } from "@angular/router";
+import { Component, OnInit } from "@angular/core";
+import { Router, RouterOutlet } from "@angular/router";
 import { FooterComponent } from "./layout/footer/footer.component";
 import { PublicHeaderComponent } from "./layout/public-header/public-header.component";
 import { PrivateHeaderComponent } from "./layout/private-header/private-header.component";
@@ -18,12 +18,24 @@ import { CommonModule } from "@angular/common";
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.scss",
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = "lab";
-
-  constructor(private authService: AuthService) {}
 
   get isLoggedIn(): boolean {
     return this.authService.isLogged();
+  }
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.checkAuthentication();
+  }
+
+  private checkAuthentication(): void {
+    if (this.authService.isLogged()) {
+      this.router.navigate(['/dashboard']);
+    } else {
+      this.router.navigate(['/']);
+    }
   }
 }
--- lab/src/app/services/auth.service.ts
+++ lab/src/app/services/auth.service.ts
@@ -5,10 +5,19 @@ export class AuthService {
   private isAuthenticated = false;
   private currentUser: string | null = null;
 
+  constructor() {
+    const storedAuthState = localStorage.getItem("authState");
+    if (storedAuthState) {
+      const { isAuthenticated, currentUser } = JSON.parse(storedAuthState);
+      this.isAuthenticated = isAuthenticated;
+      this.currentUser = currentUser;
+    }
+  }
   login({ username, password }: { username: string; password: string }): boolean {
     if (username === "master@lemoncode.net" && password === "12345678") {
       this.isAuthenticated = true;
       this.currentUser = username;
+      this.saveState();
       return true;
     }
     return false;
@@ -17,6 +26,7 @@ export class AuthService {
   logout(): void {
     this.isAuthenticated = false;
     this.currentUser = null;
+    this.clearState();
   }
 
   isLogged(): boolean {
@@ -25,5 +35,17 @@ export class AuthService {
 
   getUsername(): string {
     return this.currentUser || "";
+  }
+
+  private saveState(): void {
+    const authState = {
+      isAuthenticated: this.isAuthenticated,
+      currentUser: this.currentUser,
+    };
+    localStorage.setItem("authState", JSON.stringify(authState));
+  }
+
+  private clearState(): void {
+    localStorage.removeItem("authState");
   }
 }
